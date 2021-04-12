c8400f84ec30447123e58fa48ad814c5710e0d6b
Biliaminu <favouredbilly@gmail.com>
2021-04-12 12:08:08 +0300

    Add files via upload

--- Tweet.js
+++ Tweet.js
@@ -3,17 +3,23 @@ import './App.css'
 
 
 
-function Tweet({name,username, website}) {
-    return(
-        <div className= "tweet">
-            <h3>{name}</h3>
-            <p>{username}</p>
-            <h3>{website}</h3>
-            <button style={{ background: 'lightblue' }}>more details</button>
-            
-        </div>
+function Tweet({ name, username, website }) {
+    return (
+      <div className="tweet">
+        <h3>{name}</h3>
+        <h5>
+          <i>{"@" + username}</i>
+        </h5>
+        <h5>
+          <a href="" target="_blank">
+            {" "}
+            {"http://" + website}
+          </a>
+        </h5>
+        <a href="#" class="button">
+          MORE DETAILS
+        </a>
+      </div>
     );
-}
-
-export default Tweet;
-
+  }
+  export default Tweet;  
